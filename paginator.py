4fb6270f9d0ec67077dda8c8b5de2875c4660663
Joshua Ogden <joshua.ogden88@gmail.com>
2025-10-13 20:07:00 -0700

    finalized template pages and refactored element selection

--- paginator.py
+++ paginator.py
@@ -8,26 +8,25 @@ def load_svg(file_name):
     return ET.parse(file_name)
 
 
-def paginator(tree, max_pgn, out_dir):
+def paginator(tree, max_pgn, out_dir, side='right'):
     base_page = tree.getroot()
     # This reference to the text is no help, we need a reference to the object
     # before the .text. Only then can we update the page number and writeout.
-    base_pgn_obj = base_page \
-        .findall(PREPEND_SVG + 'g')[1] \
-        .find(PREPEND_SVG + 'text') \
-        .find(PREPEND_SVG + 'tspan')
+    base_pgn_obj = base_page.find(".//" + PREPEND_SVG + "*[@id='pgn']")[0]
     base_pgn = int(base_pgn_obj.text)
     for pgn in range(base_pgn, int(max_pgn) + 1, 2):
-        pgn_text = '{:03d}'.format(pgn)
+        pgn_text = '{: 3d}'.format(pgn) if side == 'right' else str(pgn)
+        file_name = out_dir + '{:03d}'.format(int(pgn_text.strip())) + '.svg'
+
         base_pgn_obj.text = pgn_text
-        tree.write(out_dir + '{}'.format(pgn_text) + '.svg')
+        tree.write(file_name)
 
 
 def cli(args):
-    # python3 page_numerator.py <base_page_svg> <max_page> <output_directory>
+    # <base_page_svg> <max_page> <output_directory> <page side>
     if len(args) > 3:
-        print("Opening SVG:", args[1])
-        paginator(load_svg(args[1]), args[2], args[3])
+        print("Opening SVG:", args[1], args[2], args[3], args[4])
+        paginator(load_svg(args[1]), args[2], args[3], args[4])
     else:
         print("page_numerator.py <base_page_svg> <max_page> <output_directory>")
 
